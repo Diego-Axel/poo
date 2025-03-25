81b7f278e7ea41b8afa7ee1fe88a8b5159eebac0
Diêgo Axel Bernardo Santos Rodrigues <diegoaxelbsr@gmail.com>
2025-03-24 22:31:42 -0300

    atualização de código -> CervejaApp RECEITA1

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,101 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  MaterialApp app = MaterialApp(
+  runApp(MaterialApp(
     theme: ThemeData(
-      primarySwatch: Colors.purple,
-      fontFamily: 'Roboto', // Define a fonte padrão do app
+      primarySwatch: Colors.green,
+      fontFamily: 'Roboto',
     ),
     home: Scaffold(
-      appBar: AppBar(title: Text("Meu app")),
+      appBar: AppBar(title: Text("Cervejas Bar"), backgroundColor: Colors.green),
       body: Center(
-        child: Column(
-          children: [
-            Text(
-              "Apenas começando...",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                 fontSize: 18
-                 ),
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(height: 10),
+                _buildImageWithPlaceholder(
+                  'https://i.postimg.cc/WztMvG06/cerveja-Bar.jpg',
+                ),
+                SizedBox(height: 10),
+                DataTable(
+                  columns: [
+                    DataColumn(label: Text('Nome')),
+                    DataColumn(label: Text('Estilo')),
+                    DataColumn(label: Text('IBU')),
+                  ],
+                  rows: [
+                    DataRow(cells: [
+                      DataCell(Text('La Fin Du Monde')),
+                      DataCell(Text('Bock')),
+                      DataCell(Text('65')),
+                    ]),
+                    DataRow(cells: [
+                      DataCell(Text('Sapporo Premium')),
+                      DataCell(Text('Sour Ale')),
+                      DataCell(Text('54')),
+                    ]),
+                    DataRow(cells: [
+                      DataCell(Text('Duvel')),
+                      DataCell(Text('Pilsner')),
+                      DataCell(Text('82')),
+                    ]),
+                    DataRow(cells: [
+                      DataCell(Text('Chimay Blue')),
+                      DataCell(Text('Belgian Strong Dark Ale')),
+                      DataCell(Text('35')),
+                    ]),
+                    DataRow(cells: [
+                      DataCell(Text('Westvleteren 12')),
+                      DataCell(Text('Quadrupel')),
+                      DataCell(Text('10')),
+                    ]),
+                    DataRow(cells: [
+                      DataCell(Text('Heineken')),
+                      DataCell(Text('Pilsner')),
+                      DataCell(Text('19')),
+                    ]),
+                    DataRow(cells: [
+                      DataCell(Text('Guinness')),
+                      DataCell(Text('Stout')),
+                      DataCell(Text('45')),
+                    ]),
+                    DataRow(cells: [
+                      DataCell(Text('Corona Extra')),
+                      DataCell(Text('Pale Lager')),
+                      DataCell(Text('19')),
+                    ]),
+                    DataRow(cells: [
+                      DataCell(Text('Amstel')),
+                      DataCell(Text('Pilsner')),
+                      DataCell(Text('18')),
+                    ]),
+                    DataRow(cells: [
+                      DataCell(Text('Miller Lite')),
+                      DataCell(Text('Light Lager')),
+                      DataCell(Text('10')),
+                    ]),
+                    DataRow(cells: [
+                      DataCell(Text('Budweiser')),
+                      DataCell(Text('American Lager')),
+                      DataCell(Text('12')),
+                    ]),
+                    DataRow(cells: [
+                      DataCell(Text('Peroni')),
+                      DataCell(Text('Pilsner')),
+                      DataCell(Text('20')),
+                    ]),
+                    DataRow(cells: [
+                      DataCell(Text('Weihenstephaner Hefeweissbier')),
+                      DataCell(Text('Hefeweizen')),
+                      DataCell(Text('14')),
+                    ]),
+                  ],
+                ),
+              ],
             ),
-            SizedBox(height: 10), // Espaço entre os elementos
-            _buildImageWithPlaceholder('https://i.postimg.cc/9f6CqQxW/paia.jpg'),
-            Text(
-              "No meio...",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                 fontSize: 18
-                 ),
-            ),
-            SizedBox(height: 10), // Espaço entre os elementos
-            _buildImageWithPlaceholder('https://i.postimg.cc/nLwHk10G/Captura-de-tela-2024-10-25-141130.png'),
-            Text(
-              "Terminando...",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                 fontSize: 18
-                 ),
-            ),
-          ],
+          ),
         ),
       ),
       bottomNavigationBar: Padding(
@@ -46,38 +105,59 @@ void main() {
           children: [
             ElevatedButton(
               onPressed: () {},
-              child: Icon(Icons.phone),
-            ),
-            ElevatedButton(
-              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green, // Define a cor do botão
+              ),
               child: Row(
                 mainAxisSize: MainAxisSize.min,
                 children: [
-                  Icon(Icons.star),
-                  SizedBox(width: 5),
-                  Text("Botão 2"),
+                  Icon(Icons.liquor, color: Colors.white), // Ícone branco para contraste
+                  SizedBox(width: 4),
+                  Text("Botão 1", style: TextStyle(color: Colors.white)), // Texto branco
                 ],
               ),
             ),
             ElevatedButton(
               onPressed: () {},
-              child: Icon(Icons.phone_android),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+              ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(Icons.liquor, color: Colors.white),
+                  SizedBox(width: 4),
+                  Text("Botão 2", style: TextStyle(color: Colors.white)),
+                ],
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+              ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(Icons.liquor, color: Colors.white),
+                  SizedBox(width: 4),
+                  Text("Botão 3", style: TextStyle(color: Colors.white)),
+                ],
+              ),
             ),
           ],
         ),
       ),
     ),
-  );
-  runApp(app);
+  ));
 }
 
 Widget _buildImageWithPlaceholder(String imageUrl) {
   return FadeInImage.assetNetwork(
-    placeholder: 'assets/loading.png', // Precisa de um asset local de loading
+    placeholder: 'assets/ampulheta.gif', // Precisa de um asset local de loading se você quiser
     image: imageUrl,
     width: 150,
     height: 150,
-    fit: BoxFit.cover,
     fadeInDuration: Duration(milliseconds: 500),
   );
 }
