3642a6b585d94c0b0edd882845ee59041320f5bb
Diêgo Axel Bernardo Santos Rodrigues <diegoaxelbsr@gmail.com>
2025-03-31 22:25:26 -0300

    Receita 02 Exercício 01

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,90 @@ Widget _placeHolderBuild(String imageUrl) {
 }
 
 
+class CaixaBody extends StatelessWidget {
+  CaixaBody();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: EdgeInsets.all(16),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: 10),
+              _placeHolderBuild(
+                'https://i.postimg.cc/WztMvG06/cerveja-Bar.jpg',
+              ),
+              SizedBox(height: 10),
+              DataTable(
+                columns: [
+                  DataColumn(label: Text('Nome')),
+                  DataColumn(label: Text('Estilo')),
+                  DataColumn(label: Text('IBU')),
+                ],
+                rows: [
+                  DataRow(cells: [
+                    DataCell(Text('La Fin Du Monde')),
+                    DataCell(Text('Bock')),
+                    DataCell(Text('65')),
+                  ]),
+                  DataRow(cells: [
+                    DataCell(Text('Sapporo Premium')),
+                    DataCell(Text('Sour Ale')),
+                    DataCell(Text('54')),
+                  ]),
+                  DataRow(cells: [
+                    DataCell(Text('Duvel')),
+                    DataCell(Text('Pilsner')),
+                    DataCell(Text('82')),
+                  ]),
+                  DataRow(cells: [
+                    DataCell(Text('Chimay Blue')),
+                    DataCell(Text('Belgian Strong Dark Ale')),
+                    DataCell(Text('35')),
+                  ]),
+                  DataRow(cells: [
+                    DataCell(Text('Westvleteren 12')),
+                    DataCell(Text('Quadrupel')),
+                    DataCell(Text('10')),
+                  ]),
+                  DataRow(cells: [
+                    DataCell(Text('Heineken')),
+                    DataCell(Text('Pilsner')),
+                    DataCell(Text('19')),
+                  ]),
+                  DataRow(cells: [
+                    DataCell(Text('Guinness')),
+                    DataCell(Text('Stout')),
+                    DataCell(Text('45')),
+                  ]),
+                  DataRow(cells: [
+                    DataCell(Text('Corona Extra')),
+                    DataCell(Text('Pale Lager')),
+                    DataCell(Text('19')),
+                  ]),
+                  DataRow(cells: [
+                    DataCell(Text('Amstel')),
+                    DataCell(Text('Pilsner')),
+                    DataCell(Text('18')),
+                  ]),
+                  DataRow(cells: [
+                    DataCell(Text('Miller Lite')),
+                    DataCell(Text('Light Lager')),
+                    DataCell(Text('10')),
+                  ]),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
 class CaixaBotoes extends StatelessWidget {
   CaixaBotoes();
 
@@ -50,96 +134,7 @@ void main() {
     ),
     home: Scaffold(
       appBar: AppBar(title: Text("Cervejas Bar"), backgroundColor: Colors.green),
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.all(16),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                SizedBox(height: 10),
-                _placeHolderBuild(
-                  'https://i.postimg.cc/WztMvG06/cerveja-Bar.jpg',
-                ),
-                SizedBox(height: 10),
-                DataTable(
-                  columns: [
-                    DataColumn(label: Text('Nome')),
-                    DataColumn(label: Text('Estilo')),
-                    DataColumn(label: Text('IBU')),
-                  ],
-                  rows: [
-                    DataRow(cells: [
-                      DataCell(Text('La Fin Du Monde')),
-                      DataCell(Text('Bock')),
-                      DataCell(Text('65')),
-                    ]),
-                    DataRow(cells: [
-                      DataCell(Text('Sapporo Premium')),
-                      DataCell(Text('Sour Ale')),
-                      DataCell(Text('54')),
-                    ]),
-                    DataRow(cells: [
-                      DataCell(Text('Duvel')),
-                      DataCell(Text('Pilsner')),
-                      DataCell(Text('82')),
-                    ]),
-                    DataRow(cells: [
-                      DataCell(Text('Chimay Blue')),
-                      DataCell(Text('Belgian Strong Dark Ale')),
-                      DataCell(Text('35')),
-                    ]),
-                    DataRow(cells: [
-                      DataCell(Text('Westvleteren 12')),
-                      DataCell(Text('Quadrupel')),
-                      DataCell(Text('10')),
-                    ]),
-                    DataRow(cells: [
-                      DataCell(Text('Heineken')),
-                      DataCell(Text('Pilsner')),
-                      DataCell(Text('19')),
-                    ]),
-                    DataRow(cells: [
-                      DataCell(Text('Guinness')),
-                      DataCell(Text('Stout')),
-                      DataCell(Text('45')),
-                    ]),
-                    DataRow(cells: [
-                      DataCell(Text('Corona Extra')),
-                      DataCell(Text('Pale Lager')),
-                      DataCell(Text('19')),
-                    ]),
-                    DataRow(cells: [
-                      DataCell(Text('Amstel')),
-                      DataCell(Text('Pilsner')),
-                      DataCell(Text('18')),
-                    ]),
-                    DataRow(cells: [
-                      DataCell(Text('Miller Lite')),
-                      DataCell(Text('Light Lager')),
-                      DataCell(Text('10')),
-                    ]),
-                    DataRow(cells: [
-                      DataCell(Text('Budweiser')),
-                      DataCell(Text('American Lager')),
-                      DataCell(Text('12')),
-                    ]),
-                    DataRow(cells: [
-                      DataCell(Text('Peroni')),
-                      DataCell(Text('Pilsner')),
-                      DataCell(Text('20')),
-                    ]),
-                    DataRow(cells: [
-                      DataCell(Text('Weihenstephaner Hefeweissbier')),
-                      DataCell(Text('Hefeweizen')),
-                      DataCell(Text('14')),
-                    ]),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+      body: CaixaBody(),
       bottomNavigationBar: CaixaBotoes(),
     ),
   );
