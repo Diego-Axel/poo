613052d6bc30389d52e5d2f669381f2fef4a91e2
Diêgo Axel Bernardo Santos Rodrigues <diegoaxelbsr@gmail.com>
2025-04-24 22:55:33 -0300

    DESAFIO FABRICIO
    Esse foi um desafio proposto por Fabricio, onde, ao tocar no botão lateral do app, teria as cores e quando o usuário clicar em uma das três cores, a cor principal do app mudaria para a cor quer ele selecionasse.

--- lib/main.dart
+++ lib/main.dart
@@ -1,132 +1,142 @@
 import 'package:flutter/material.dart';
 
-
 void main() {
-
-  MyApp app = MyApp();
-
-  runApp(app);
-
+  runApp(MyApp());
 }
 
-
-
-class MyApp extends StatelessWidget {
-
+class MyApp extends StatefulWidget {
   @override
+  State<MyApp> createState() => _MyAppState();
+}
 
-  Widget build(BuildContext context) {
+class _MyAppState extends State<MyApp> {
+  MaterialColor _primaryColor = Colors.deepPurple;
 
-    return MaterialApp(
-
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
-
-      debugShowCheckedModeBanner:false,
-
-      home: Scaffold(
-
-        appBar: AppBar( 
-
-          title: const Text("Dicas"),
-
-          ),
-
-        body: DataBodyWidget(objects:[
-
-          "La Fin Du Monde - Bock - 65 ibu",
-
-          "Sapporo Premiume - Sour Ale - 54 ibu",
-
-          "Duvel - Pilsner - 82 ibu"
-
-        ]),
-
-        bottomNavigationBar: NewNavBar(),
-
-      ));
-
+  void _changeTheme(String colorName) {
+    setState(() {
+      switch (colorName) {
+        case "Verde":
+          _primaryColor = Colors.green;
+          break;
+        case "Laranja":
+          _primaryColor = Colors.orange;
+          break;
+        case "Roxo":
+        default:
+          _primaryColor = Colors.deepPurple;
+      }
+    });
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: _primaryColor),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: MyAppBar(onColorSelected: _changeTheme),
+        body: DataBodyWidget(objects: [
+          "La Fin Du Monde - Bock - 65 ibu",
+          "Sapporo Premiume - Sour Ale - 54 ibu",
+          "Duvel - Pilsner - 82 ibu"
+        ]),
+        bottomNavigationBar: NewNavBar(icons: const [
+          Icons.coffee_outlined,
+          Icons.local_drink_outlined,
+          Icons.flag_outlined
+        ]),
+      ),
+    );
+  }
 }
 
+// AppBar com callback
+class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final void Function(String)? onColorSelected;
 
+  const MyAppBar({this.onColorSelected});
 
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: const Text("Dicas"),
+      actions: [
+        PopupMenuButton<String>(
+          onSelected: (String value) {
+            print("Selecionou a cor: $value");
+            if (onColorSelected != null) {
+              onColorSelected!(value);
+            }
+          },
+          itemBuilder: (BuildContext context) {
+            return const [
+              PopupMenuItem<String>(
+                value: "Roxo",
+                child: Text("Roxo"),
+              ),
+              PopupMenuItem<String>(
+                value: "Verde",
+                child: Text("Verde"),
+              ),
+              PopupMenuItem<String>(
+                value: "Laranja",
+                child: Text("Laranja"),
+              ),
+            ];
+          },
+        ),
+      ],
+    );
+  }
 
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
 
+// As demais classes permanecem idênticas
 class NewNavBar extends StatelessWidget {
+  final List<IconData> icons;
 
-  NewNavBar();
+  NewNavBar({this.icons = const []});
 
   void botaoFoiTocado(int index) {
-
     print("Tocaram no botão $index");
-
   }
 
-
+  BottomNavigationBarItem construirItem(IconData icone) {
+    return BottomNavigationBarItem(
+      icon: Icon(icone),
+      label: "",
+    );
+  }
 
   @override
-
   Widget build(BuildContext context) {
+    List<BottomNavigationBarItem> items =
+        icons.map((icone) => construirItem(icone)).toList();
 
-    return BottomNavigationBar(onTap: botaoFoiTocado, items: const [
-
-      BottomNavigationBarItem(
-
-        label: "Cafés",
-
-        icon: Icon(Icons.coffee_outlined),
-
-      ),
-
-      BottomNavigationBarItem(
-
-          label: "Cervejas", icon: Icon(Icons.local_drink_outlined)),
-
-      BottomNavigationBarItem(label: "Nações", icon: Icon(Icons.flag_outlined))
-
-    ]);
-
+    return BottomNavigationBar(
+      onTap: botaoFoiTocado,
+      items: items,
+    );
   }
-
 }
 
-
-
 class DataBodyWidget extends StatelessWidget {
-
   List<String> objects;
 
-  DataBodyWidget( {this.objects = const [] });
+  DataBodyWidget({this.objects = const []});
 
-
-
-  Expanded processarUmElemento(String obj){
-
-    return Expanded(                
-
-          child: Center(child: Text(obj)),
-
-        );
-
+  Expanded processarUmElemento(String obj) {
+    return Expanded(
+      child: Center(child: Text(obj)),
+    );
   }
-
-
 
   @override
-
   Widget build(BuildContext context) {
-
-    return Column(children: objects.map( 
-
-      (obj) => Expanded(
-
-        child: Center(child: Text(obj)),
-
-        )
-
-      ).toList());
-
+    List<Expanded> allTheLines =
+        objects.map((obj) => processarUmElemento(obj)).toList();
+    return Column(children: allTheLines);
   }
-
 }
