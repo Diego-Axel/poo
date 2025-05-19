3c4fd0b96e0e20d408b78a22c43a73f63b007def
Diêgo Axel Bernardo Santos Rodrigues <diegoaxelbsr@gmail.com>
2025-05-19 14:10:01 -0300

    código inicial RECEITA 06

--- lib/main.dart
+++ lib/main.dart
@@ -1,93 +1,172 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+
 
 var dataObjects = [];
 
+
+
 void main() {
+
   MyApp app = MyApp();
+
   runApp(app);
+
 }
+
+
 
 class MyApp extends StatelessWidget {
+
+
+
   @override
+
   Widget build(BuildContext context) {
-    print("no build da classe MyApp");
 
     return MaterialApp(
+
       theme: ThemeData(primarySwatch: Colors.deepPurple),
-      debugShowCheckedModeBanner: false,
+
+      debugShowCheckedModeBanner:false,
+
       home: Scaffold(
-        appBar: AppBar(
+
+        appBar: AppBar( 
+
           title: const Text("Dicas"),
-        ),
-        body: DataTableWidget(jsonObjects: dataObjects),
-        bottomNavigationBar: NewNavBar2(),
-      ),
-    );
+
+          ),
+
+        body: DataTableWidget(jsonObjects:dataObjects),
+
+        bottomNavigationBar: NewNavBar(),
+
+      ));
+
   }
+
+
+
 }
 
-class NewNavBar2 extends StatefulWidget {
-  @override
-  _NewNavBar2State createState() => _NewNavBar2State();
-}
 
-class _NewNavBar2State extends State<NewNavBar2> {
-  int selectedIndex = 1;
+
+
+
+class NewNavBar extends HookWidget {
+
+  NewNavBar();
+
+
 
   @override
+
   Widget build(BuildContext context) {
-    print("no build da classe NewNavBar2");
+
+    var state = useState(1);
 
     return BottomNavigationBar(
-      onTap: (index) {
-        setState(() {
-          selectedIndex = index;
-        });
-      },
-      currentIndex: selectedIndex,
+
+      onTap: (index){
+
+        state.value = index;
+
+      }, 
+
+      currentIndex: state.value,
+
       items: const [
+
         BottomNavigationBarItem(
+
           label: "Cafés",
+
           icon: Icon(Icons.coffee_outlined),
+
         ),
+
+
+
         BottomNavigationBarItem(
-          label: "Cervejas",
-          icon: Icon(Icons.local_drink_outlined),
-        ),
-        BottomNavigationBarItem(
-          label: "Nações",
-          icon: Icon(Icons.flag_outlined),
-        ),
-      ],
-    );
+
+            label: "Cervejas", icon: Icon(Icons.local_drink_outlined)),
+
+
+
+        BottomNavigationBarItem(label: "Nações", icon: Icon(Icons.flag_outlined))
+
+      ]);
+
+
+
   }
+
+
+
 }
 
+
+
+
+
+
+
 class DataTableWidget extends StatelessWidget {
+
+
+
   final List jsonObjects;
 
-  DataTableWidget({this.jsonObjects = const []});
+  final List<String> columnNames;
+
+  final List<String> propertyNames;
+
+
+
+  DataTableWidget( {this.jsonObjects = const [], this.columnNames = const ["Nome","Estilo","IBU"], this.propertyNames= const ["name", "style", "ibu"]});
+
+
 
   @override
-  Widget build(BuildContext context) {
-    print("no build da classe DataTableWidget");
 
-    var columnNames = ["Nome", "Estilo", "IBU"],
-        propertyNames = ["name", "style", "ibu"];
+  Widget build(BuildContext context) {
 
     return DataTable(
-      columns: columnNames
-          .map((name) => DataColumn(
+
+      columns: columnNames.map( 
+
+                (name) => DataColumn(
+
                   label: Expanded(
-                      child: Text(name,
-                          style: TextStyle(fontStyle: FontStyle.italic)))))
-          .toList(),
-      rows: jsonObjects
-          .map((obj) => DataRow(
-                  cells: propertyNames
-                      .map((propName) => DataCell(Text(obj[propName])))
-                      .toList()))
-          .toList(),
-    );
+
+                    child: Text(name, style: TextStyle(fontStyle: FontStyle.italic))
+
+                  )
+
+                )
+
+              ).toList()       
+
+      ,
+
+      rows: jsonObjects.map( 
+
+        (obj) => DataRow(
+
+            cells: propertyNames.map(
+
+              (propName) => DataCell(Text(obj[propName]))
+
+            ).toList()
+
+          )
+
+        ).toList());
+
   }
+
+
+
 }
