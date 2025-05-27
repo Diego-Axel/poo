4176bf88076b558c15ae20de8781763b6ecdf206
Diêgo Axel Bernardo Santos Rodrigues <diegoaxelbsr@gmail.com>
2025-05-27 12:26:53 -0300

    montando uma interface simples para colocar as receitas

---
+++ lib/recipes/recipe04.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Recipe04 extends StatelessWidget {
+  const Recipe04({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Receita 04'),
+      ),
+      body: const Center(
+        child: Text('Aqui vai o código da Receita 04'),
+      ),
+    );
+  }
+}
---
+++ lib/recipes/recipe06.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Recipe06 extends StatelessWidget {
+  const Recipe06({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Receita 06'),
+      ),
+      body: const Center(
+        child: Text('Aqui vai o código da Receita 06'),
+      ),
+    );
+  }
+}
