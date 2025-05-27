25409dbb55d82b7ec9dcaa281c3c250a66bfc76e
Diêgo Axel Bernardo Santos Rodrigues <diegoaxelbsr@gmail.com>
2025-05-27 12:40:33 -0300

    Colocando o código da resolução da receita 04

--- lib/recipes/recipe04.dart
+++ lib/recipes/recipe04.dart
@@ -1,17 +1,69 @@
 import 'package:flutter/material.dart';
 
 class Recipe04 extends StatelessWidget {
-  const Recipe04({Key? key}) : super(key: key);
+  const Recipe04({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var coffeeObjects = [
+      {"brand": "Illy", "type": "Espresso", "intensity": "Strong"},
+      {"brand": "Nespresso", "type": "Lungo", "intensity": "Medium"},
+      {"brand": "Pilão", "type": "Tradicional", "intensity": "Strong"},
+      {"brand": "Melitta", "type": "Coador", "intensity": "Mild"},
+    ];
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Receita 04'),
+        title: const Text("Tabela Genérica"),
       ),
-      body: const Center(
-        child: Text('Aqui vai o código da Receita 04'),
+      body: DataBodyWidget(
+        objects: coffeeObjects,
+        columnNames: const ["Marca", "Tipo", "Intensidade"],
+        propertyNames: const ["brand", "type", "intensity"],
       ),
     );
   }
 }
+
+class DataBodyWidget extends StatelessWidget {
+  final List<Map<String, dynamic>> objects;
+  final List<String> columnNames;
+  final List<String> propertyNames;
+
+  const DataBodyWidget({
+    super.key,
+    required this.objects,
+    required this.columnNames,
+    required this.propertyNames,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: DataTable(
+        columns: columnNames
+            .map(
+              (name) => DataColumn(
+                label: Expanded(
+                  child: Text(
+                    name,
+                    style: const TextStyle(fontStyle: FontStyle.italic),
+                  ),
+                ),
+              ),
+            )
+            .toList(),
+        rows: objects.map((obj) {
+          return DataRow(
+            cells: propertyNames.map((propName) {
+              var cellValue = obj[propName]?.toString() ?? "";
+              return DataCell(Text(cellValue));
+            }).toList(),
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
+
